00a294c538c2a8b3a1e11eb261612cb6688d70d8
zigoro <ken.sony.xperia.0103@gmail.com>
2021-05-30 10:25:19 +0900

    add alt

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = (props) => {
         <nav class="navbar" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="logo" />
                 </a>
 
                 <a href="/" role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
