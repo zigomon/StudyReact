189f4e2dbab2f5d0bd5a053d963bebd89d6d6c09
zigoro <ken.sony.xperia.0103@gmail.com>
2021-05-30 10:19:33 +0900

    add href

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = (props) => {
                     <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
                 </a>
 
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a href="/" role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -17,31 +17,31 @@ const Navbar = (props) => {
 
             <div id="navbarBasicExample" class="navbar-menu">
                 <div class="navbar-start">
-                    <a class="navbar-item">
+                    <a class="navbar-item" href="/">
                         Home
                     </a>
 
-                    <a class="navbar-item">
+                    <a class="navbar-item" href="/">
                         Documentation
                     </a>
 
                     <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
-                        More
+                        <a class="navbar-link" href="/">
+                            More
                         </a>
 
                         <div class="navbar-dropdown">
-                            <a class="navbar-item">
+                            <a class="navbar-item" href="/">
                                 About
                             </a>
-                            <a class="navbar-item">
+                            <a class="navbar-item" href="/">
                                 Jobs
                             </a>
-                            <a class="navbar-item">
+                            <a class="navbar-item" href="/">
                                 Contact
                             </a>
                             <hr class="navbar-divider" />
-                            <a class="navbar-item">
+                            <a class="navbar-item" href="/">
                                 Report an issue
                             </a>
                         </div>
@@ -51,10 +51,10 @@ const Navbar = (props) => {
                 <div class="navbar-end">
                     <div class="navbar-item">
                         <div class="buttons">
-                            <a class="button is-primary">
+                            <a class="button is-primary" href="/">
                                 <strong>Sign up</strong>
                             </a>
-                            <a class="button is-light">
+                            <a class="button is-light" href="/">
                                 Log in
                             </a>
                         </div>
