622d9f3a881616088d33fd3a8ff8eb60bcdb7d9f
zigoro <ken.sony.xperia.0103@gmail.com>
2021-06-06 07:09:51 +0900

    Add Api

---
+++ api/EnvironmentDef.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZigomonzApps
+{
+    public static class EnvironmentDef
+    {
+        public const string APPID_USAGE = "UsageApp";
+
+
+
+        public const string KEY_CONNECTIONSTRING_COSMOSDB = "ConnectionStringCosmosDB";
+    }
+}
